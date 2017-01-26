626631ca4fda4e8f64b167b637eed5fb28822751
paulfelce <paulfelce@gmail.com>
2017-01-26 13:49:42 +0000

    Refactored script to it's own file

---
+++ th.js
@@ -0,0 +1,36 @@
+
+var hidden = false;
+var qrcode1 ; 
+var qrcode2 ; 
+var qrcode3 ; 
+var qrcode4 ; 
+var qrcode5 ; 
+var qrcode6 ;
+$(function(){
+qrcode1 = new QRCode("qrcode1");
+qrcode2 = new QRCode("qrcode2");
+qrcode3 = new QRCode("qrcode3");
+qrcode4 = new QRCode("qrcode4");
+qrcode5 = new QRCode("qrcode5");
+qrcode6 = new QRCode("qrcode6");
+});
+function toggle(){
+  if(hidden){
+    $("#text1").show()
+    $("#text2").show()
+    $("#text3").show()
+    $("#text4").show()
+    $("#text5").show()
+    $("#text6").show()
+  }
+  else
+    {
+      $("#text1").hide()
+      $("#text2").hide()
+      $("#text3").hide()
+      $("#text4").hide()
+      $("#text5").hide()
+      $("#text6").hide()
+    }
+  hidden = !hidden
+}
